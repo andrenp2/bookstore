93619e96e9f8003a12c17777f5f9e780a01b7487
Andre Pavan <dehnuernberg@hotmail.com>
2024-09-29 10:07:40 -0300

    corrigindo arquivos e criando os testes viewsets

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
+from order.models.order import Order
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
-from order.models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(required=True, many=True)
+    product = ProductSerializer(read_only=True, many=True)
     products_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True, many=True)
     total = serializers.SerializerMethodField()
 
@@ -16,15 +16,14 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['product', 'total', 'user', 'products_id']
-        extra_kawargs = {'product' : {'required': False}}
+        extra_kwargs = {'product': {'required' : False}}
 
     def create(self, validated_data):
         product_data = validated_data.pop('products_id')
         user_data = validated_data.pop('user')
 
-        order = Order.objects.create(**validated_data)
+        order = Order.objects.create(user=user_data)
         for product in product_data:
             order.product.add(product)
 
         return order
-
--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 
 from product.models.category import Category
 
+
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = [
@@ -11,3 +13,4 @@ class CategorySerializer(serializers.ModelSerializer):
             'description',
             'active',
         ]
+        extra_kwargs = {'slug': {'required': False}}
